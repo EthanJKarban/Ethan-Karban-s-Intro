532e6014727ecb01bdde1815d1e6e7fce2cc8237
MUKILTEO\1721000 <1721000@mukilteo.wednet.edu>
2025-09-11 11:48:12 -0700

    T_T I feel like it's so incomplete, but at least I got to add a bunny

--- 2D Template/Assets/Scripts/MovePlat.cs
+++ 2D Template/Assets/Scripts/MovePlat.cs
@@ -23,6 +23,7 @@ public class MovePlat : MonoBehaviour
         if(transform.position == nextPosition)
         {
             nextPosition = (nextPosition == pointA.position) ? pointB.position : pointA.position;
+            return;
         }
     }
 }
